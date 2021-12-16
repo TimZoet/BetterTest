c130044a9cbbe198139a45708871b2aed56b1df2
Tim Zoet <ts.zoet@gmail.com>
2021-12-16 19:57:11 +0100

    Greatly improved packaging and conan integration

--- conanfile.py
+++ conanfile.py
@@ -1,56 +1,88 @@
-import re
-
 from conans import ConanFile
-from conans.tools import load
-from conan.tools.cmake import CMakeToolchain, CMake
-from conan.tools.layout import cmake_layout
-
-def get_version():
-    try:
-        content = load("modules/bettertest/bettertestVersionString.cmake")
-        version = re.search("set\(BETTERTEST_VERSION (\d+\.\d+\.\d+)\)", content).group(1)
-        return version.strip()
-    except Exception as e:
-        return None
 
 class BetterTestConan(ConanFile):
-    name = "BetterTest"
-    version = get_version()
-    requires = "common/1.0.0@timzoet/stable", "date/3.0.1", "math/1.0.0@timzoet/stable", "nlohmann_json/3.9.1", "ParserTongue/1.1.0@timzoet/stable", "pugixml/1.11"
-    generators = "cmake_find_package"
-
-    # Optional metadata
-    license = "GNU AFFERO GENERAL PUBLIC LICENSE Version 3"
-    author = "Tim Zoet"
-    url = "https://github.com/TimZoet/BetterTest"
+    ############################################################################
+    ## Package info.                                                          ##
+    ############################################################################
+    
+    name = "bettertest"
+    
     description = "C++ testing framework."
+    
+    url = "https://github.com/TimZoet/BetterTest"
 
-    # Binary configuration
-    settings = "os", "compiler", "build_type", "arch"
-    options = {"fPIC": [True, False]}
-    default_options = {"fPIC": True}
+    ############################################################################
+    ## Settings.                                                              ##
+    ############################################################################
 
-    exports_sources = "CMakeLists.txt", "readme.md", "cmake/*", "modules/CMakeLists.txt", "modules/bettertest/*"
-
+    python_requires = "pyreq/1.0.0@timzoet/stable"
+    
+    python_requires_extend = "pyreq.BaseConan"
+    
+    ############################################################################
+    ## Base methods.                                                          ##
+    ############################################################################
+    
+    def set_version(self):
+        base = self.python_requires["pyreq"].module.BaseConan
+        base.set_version(self, "modules/bettertest/bettertestVersionString.cmake", "BETTERTEST_VERSION")
+    
+    def init(self):
+        base = self.python_requires["pyreq"].module.BaseConan
+        self.generators = base.generators + self.generators
+        self.settings = base.settings + self.settings
+        self.options = {**base.options, **self.options}
+        self.default_options = {**base.default_options, **self.default_options}
+    
+    ############################################################################
+    ## Building.                                                              ##
+    ############################################################################
+    
+    def export_sources(self):
+        self.copy("CMakeLists.txt")
+        self.copy("license")
+        self.copy("readme.md")
+        self.copy("cmake/*")
+        self.copy("modules/CMakeLists.txt")
+        self.copy("modules/bettertest/*")
+    
     def config_options(self):
+        base = self.python_requires["pyreq"].module.BaseConan
         if self.settings.os == "Windows":
             del self.options.fPIC
-
-    def layout(self):
-        cmake_layout(self)
-
+    
+    def requirements(self):
+        base = self.python_requires["pyreq"].module.BaseConan
+        base.requirements(self)
+        
+        self.requires("common/1.0.0@timzoet/stable")
+        self.requires("date/3.0.1")
+        self.requires("nlohmann_json/3.9.1")
+        self.requires("parsertongue/1.1.0@timzoet/stable")
+        self.requires("pugixml/1.11")
+    
+    def package_info(self):
+        self.cpp_info.libs = ["bettertest"]
+    
     def generate(self):
-        tc = CMakeToolchain(self)
+        base = self.python_requires["pyreq"].module.BaseConan
+        
+        tc = base.generate_toolchain(self)
         tc.generate()
+        
+        deps = base.generate_deps(self)
+        deps.generate()
 
     def build(self):
-        cmake = CMake(self)
+        base = self.python_requires["pyreq"].module.BaseConan
+        cmake = base.configure_cmake(self)
         cmake.configure()
         cmake.build()
 
     def package(self):
-        cmake = CMake(self)
+        base = self.python_requires["pyreq"].module.BaseConan
+        cmake = base.configure_cmake(self)
         cmake.install()
 
-    def package_info(self):
-        self.cpp_info.libs = ["bettertest"]
+
+

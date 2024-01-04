dd8881cbbc79b22c023075bd2431514f3a4e37a5
TimZoet <ts.zoet@gmail.com>
2024-01-04 21:40:03 +0100

    Changed the vector-only compareEQ into a generic range comparison function.

--- modules/bettertest/include/bettertest/mixins/compare_mixin.h
+++ modules/bettertest/include/bettertest/mixins/compare_mixin.h
@@ -5,6 +5,8 @@
 ////////////////////////////////////////////////////////////////
 
 #include <format>
+#include <functional>
+#include <ranges>
 #include <string>
 #include <source_location>
 
@@ -27,7 +29,21 @@ namespace bt
     class CompareMixin : public IMixin
     {
     public:
+        ////////////////////////////////////////////////////////////////
+        // Types.
+        ////////////////////////////////////////////////////////////////
+
         static constexpr char type[] = "compare";
+
+        enum class RequireEqualLength
+        {
+            False = 0,
+            True  = 1
+        };
+
+        ////////////////////////////////////////////////////////////////
+        // Constructors.
+        ////////////////////////////////////////////////////////////////
 
         CompareMixin() = default;
 
@@ -52,7 +68,14 @@ namespace bt
         // Comparisons.
         ////////////////////////////////////////////////////////////////
 
-        template<typename A>
+        /**
+         * \brief lhs == true. Compare a value to true. Does not do implicit conversions to bool.
+         * \tparam A Type of left-hand side.
+         * \param lhs Left-hand side of comparison.
+         * \param loc Automatic source_location.
+         * \return Result.
+         */
+        template<type_traits::comparable_eq<bool> A>
         CheckResult compareTrue(const A& lhs, const std::source_location& loc = std::source_location::current())
         {
             const auto  res   = compare_true(lhs);
@@ -60,7 +83,14 @@ namespace bt
             return recordResult(res, loc, std::move(error));
         }
 
-        template<typename A>
+        /**
+         * \brief lhs == false. Compare a value to false. Does not do implicit conversions to bool.
+         * \tparam A Type of left-hand side.
+         * \param lhs Left-hand side of comparison.
+         * \param loc Automatic source_location.
+         * \return Result.
+         */
+        template<type_traits::comparable_ne<bool> A>
         CheckResult compareFalse(const A& lhs, const std::source_location& loc = std::source_location::current())
         {
             const auto  res   = compare_false(lhs);
@@ -68,42 +98,176 @@ namespace bt
             return recordResult(res, loc, std::move(error));
         }
 
-        template<typename A, typename B>
-        CheckResult
-          compareEQ(const A& lhs, const B& rhs, const std::source_location& loc = std::source_location::current())
+        /**
+         * \brief lhs == rhs. Compare two values for equality.
+         * \tparam A Type of left-hand side.
+         * \tparam B Type of right-hand side.
+         * \param lhs Left-hand side of comparison.
+         * \param rhs Right-hand side of comparison.
+         * \param loc Automatic source_location.
+         * \return Result.
+         */
+        template<typename A, typename B, typename ProjA = std::identity, typename ProjB = std::identity>
+            requires(!std::ranges::range<A> &&
+                     type_traits::comparable_eq<std::invoke_result_t<ProjA, A>, std::invoke_result_t<ProjB, B>>)
+        CheckResult compareEQ(const A&                    lhs,
+                              const B&                    rhs,
+                              ProjA                       projectA = std::identity{},
+                              ProjB                       projectB = std::identity{},
+                              const std::source_location& loc      = std::source_location::current())
         {
-            const auto  res   = compare_eq(lhs, rhs);
+            const auto  res   = compare_eq(projectA(lhs), projectB(rhs));
             std::string error = res == result_t::success ? "" : std::format("{0} == {1}", toString(lhs), toString(rhs));
             return recordResult(res, loc, std::move(error));
         }
 
-        template<typename A, typename B>
-        requires(is_vector_v<A>&& is_vector_v<B>) CheckResult
-          compareEQ(const A& lhs, const B& rhs, const std::source_location& loc = std::source_location::current())
+        /**
+         * \brief lhs[0] == rhs[0], ..., lhs[n] == rhs[n]. Compare the elements of two ranges for equality.
+         * \tparam Length If true, mismatching lengths result in a failure. If false, the extra elements of the longest range are ignored.
+         * \tparam A Type of left-hand side.
+         * \tparam B Type of right-hand side.
+         * \tparam ProjA Projection function type for lhs.
+         * \tparam ProjB Projection function type for rhs.
+         * \param lhs Left-hand side of comparison.
+         * \param rhs Right-hand side of comparison.
+         * \param projectLhs Projection function type to be applied to elements of lhs.
+         * \param projectRhs Projection function type to be applied to elements of rhs.
+         * \param loc Automatic source_location.
+         * \return Result.
+         */
+        template<RequireEqualLength         Length = RequireEqualLength::True,
+                 std::ranges::forward_range A,
+                 std::ranges::forward_range B,
+                 typename ProjA = std::identity,
+                 typename ProjB = std::identity>
+            requires(
+              type_traits::comparable_eq<std::invoke_result_t<ProjA, typename std::ranges::iterator_t<A>::value_type>,
+                                         std::invoke_result_t<ProjB, typename std::ranges::iterator_t<B>::value_type>>)
+        CheckResult compareEQ(const A&                    lhs,
+                              const B&                    rhs,
+                              ProjA                       projectLhs = std::identity{},
+                              ProjB                       projectRhs = std::identity{},
+                              const std::source_location& loc        = std::source_location::current())
         {
-            // First compare sizes.
-            if (lhs.size() != rhs.size())
-            {
-                constexpr auto res   = result_t::failure;
-                std::string    error = std::format("size {0} == {1}", lhs.size(), rhs.size());
-                return recordResult(res, loc, std::move(error));
-            }
+            // Limit iterations to 4 billion in the case of infinite ranges.
+            constexpr size_t limit = 1ull << 32;
 
-            // Then compare elements.
-            for (size_t i = 0; i < lhs.size(); i++)
+            auto       it0  = std::ranges::begin(lhs);
+            auto       it1  = std::ranges::begin(rhs);
+            const auto end0 = std::ranges::end(lhs);
+            const auto end1 = std::ranges::end(rhs);
+
+            // If supported, get size of ranges.
+            std::string size0 = "?";
+            std::string size1 = "?";
+            if constexpr (std::ranges::sized_range<A>) size0 = std::to_string(std::distance(it0, end0));
+            if constexpr (std::ranges::sized_range<B>) size1 = std::to_string(std::distance(it1, end1));
+
+            size_t i = 0;
+            while (i < limit)
             {
-                const auto res = compare_eq(lhs[i], rhs[i]);
+                // Reached end of one of the ranges.
+                if (it0 == end0 || it1 == end1)
+                {
+                    // Matching sizes.
+                    if (it0 == end0 && it1 == end1) return recordResult(result_t::success, loc, "");
+
+                    // Mismatching sizes.
+                    if constexpr (Length == RequireEqualLength::True)
+                        return recordResult(
+                          result_t::failure, loc, std::format("length lhs[{0}] != length rhs[{1}]", size0, size1));
+                    // Don't care.
+                    else
+                        return recordResult(result_t::success, loc, "");
+                }
+
+                const auto res = compare_eq(projectLhs(*it0), projectRhs(*it1));
                 if (res != result_t::success)
                 {
-                    std::string error = std::format("index[{0}] {1} == {2}", i, toString(lhs[i]), toString(rhs[i]));
+                    std::string error =
+                      std::format("{1} == {2} at index {0}", i, toString(projectLhs(*it0)), toString(projectRhs(*it1)));
                     return recordResult(res, loc, std::move(error));
                 }
+
+                ++it0;
+                ++it1;
+                ++i;
             }
 
-            return recordResult(result_t::success, loc, "");
+            return recordResult(result_t::failure, loc, std::format("range comparison exceeded limit {}", limit));
         }
 
+        /**
+         * \brief lhs == rhs[0], ..., lhs == rhs[n]. Compare a single value to the elements of a range for equality.
+         * \tparam A Type of left-hand side.
+         * \tparam B Type of right-hand side.
+         * \tparam ProjA Projection function type for lhs.
+         * \tparam ProjB Projection function type for rhs.
+         * \param lhs Left-hand side of comparison.
+         * \param rhs Right-hand side of comparison.
+         * \param projectLhs Projection function type to be applied to elements of lhs.
+         * \param projectRhs Projection function type to be applied to elements of rhs.
+         * \param loc Automatic source_location.
+         * \return Result.
+         */
+        template<typename A,
+                 std::ranges::forward_range B,
+                 typename ProjA = std::identity,
+                 typename ProjB = std::identity>
+            requires(
+              type_traits::comparable_eq<A,
+                                         std::invoke_result_t<ProjB, typename std::ranges::iterator_t<B>::value_type>>)
+        CheckResult compareEQ(const A&                    lhs,
+                              const B&                    rhs,
+                              ProjA&&                     projectLhs = std::identity{},
+                              ProjB&&                     projectRhs = std::identity{},
+                              const std::source_location& loc        = std::source_location::current())
+        {
+            return compareEQ<RequireEqualLength::False>(
+              std::views::repeat(lhs), rhs, std::forward<ProjA>(projectLhs), std::forward<ProjB>(projectRhs), loc);
+        }
+
+        /**
+         * \brief lhs[0] == rhs, ..., lhs[0] == rhs. Compare a single value to the elements of a range for equality.
+         * \tparam A Type of left-hand side.
+         * \tparam B Type of right-hand side.
+         * \tparam ProjA Projection function type for lhs.
+         * \tparam ProjB Projection function type for rhs.
+         * \param lhs Left-hand side of comparison.
+         * \param rhs Right-hand side of comparison.
+         * \param projectLhs Projection function type to be applied to elements of lhs.
+         * \param projectRhs Projection function type to be applied to elements of rhs.
+         * \param loc Automatic source_location.
+         * \return Result.
+         */
+        template<std::ranges::forward_range A,
+                 typename B,
+                 typename ProjA = std::identity,
+                 typename ProjB = std::identity>
+            requires(
+              type_traits::comparable_eq<std::invoke_result_t<ProjA, typename std::ranges::iterator_t<A>::value_type>,
+                                         B>)
+        CheckResult compareEQ(const A&                    lhs,
+                              const B&                    rhs,
+                              ProjA&&                     projectLhs = std::identity{},
+                              ProjB&&                     projectRhs = std::identity{},
+                              const std::source_location& loc        = std::source_location::current())
+        {
+            return compareEQ<RequireEqualLength::False>(
+              lhs, std::views::repeat(rhs), std::forward<ProjA>(projectLhs), std::forward<ProjB>(projectRhs), loc);
+        }
+
+        /**
+         * \brief lhs != rhs. Compare two values for inequality.
+         * \tparam A Type of left-hand side.
+         * \tparam B Type of right-hand side.
+         * \param lhs Left-hand side of comparison.
+         * \param rhs Right-hand side of comparison.
+         * \param loc Automatic source_location.
+         * \return Result.
+         */
         template<typename A, typename B>
+            requires(type_traits::comparable_ne<A, B>)
         CheckResult
           compareNE(const A& lhs, const B& rhs, const std::source_location& loc = std::source_location::current())
         {
@@ -112,7 +276,17 @@ namespace bt
             return recordResult(res, loc, std::move(error));
         }
 
+        /**
+         * \brief lhs < rhs. Compare two values.
+         * \tparam A Type of left-hand side.
+         * \tparam B Type of right-hand side.
+         * \param lhs Left-hand side of comparison.
+         * \param rhs Right-hand side of comparison.
+         * \param loc Automatic source_location.
+         * \return Result.
+         */
         template<typename A, typename B>
+            requires(type_traits::comparable_lt<A, B>)
         CheckResult
           compareLT(const A& lhs, const B& rhs, const std::source_location& loc = std::source_location::current())
         {
@@ -121,7 +295,17 @@ namespace bt
             return recordResult(res, loc, std::move(error));
         }
 
+        /**
+         * \brief lhs > rhs. Compare two values.
+         * \tparam A Type of left-hand side.
+         * \tparam B Type of right-hand side.
+         * \param lhs Left-hand side of comparison.
+         * \param rhs Right-hand side of comparison.
+         * \param loc Automatic source_location.
+         * \return Result.
+         */
         template<typename A, typename B>
+            requires(type_traits::comparable_gt<A, B>)
         CheckResult
           compareGT(const A& lhs, const B& rhs, const std::source_location& loc = std::source_location::current())
         {
@@ -130,7 +314,17 @@ namespace bt
             return recordResult(res, loc, std::move(error));
         }
 
+        /**
+         * \brief lhs <= rhs. Compare two values.
+         * \tparam A Type of left-hand side.
+         * \tparam B Type of right-hand side.
+         * \param lhs Left-hand side of comparison.
+         * \param rhs Right-hand side of comparison.
+         * \param loc Automatic source_location.
+         * \return Result.
+         */
         template<typename A, typename B>
+            requires(type_traits::comparable_le<A, B>)
         CheckResult
           compareLE(const A& lhs, const B& rhs, const std::source_location& loc = std::source_location::current())
         {
@@ -139,7 +333,17 @@ namespace bt
             return recordResult(res, loc, std::move(error));
         }
 
+        /**
+         * \brief lhs >= rhs. Compare two values.
+         * \tparam A Type of left-hand side.
+         * \tparam B Type of right-hand side.
+         * \param lhs Left-hand side of comparison.
+         * \param rhs Right-hand side of comparison.
+         * \param loc Automatic source_location.
+         * \return Result.
+         */
         template<typename A, typename B>
+            requires(type_traits::comparable_ge<A, B>)
         CheckResult
           compareGE(const A& lhs, const B& rhs, const std::source_location& loc = std::source_location::current())
         {
@@ -148,11 +352,25 @@ namespace bt
             return recordResult(res, loc, std::move(error));
         }
 
+        /**
+         * \brief lower <[=] middle <[=] upper. Compare middle value with lower and upper.
+         * \tparam LowerInclusive If true, do lower <= middle. If false, do power < middle.
+         * \tparam UpperInclusive If true, do middle <= upper. If false, do middle < upper.
+         * \tparam A Type of lower.
+         * \tparam B Type of middle.
+         * \tparam C Type of upper.
+         * \param lower Lower value of comparison.
+         * \param middle Middle value of comparison.
+         * \param upper Upper value of comparison.
+         * \param loc Automatic source_location.
+         * \return Result.
+         */
         template<bool LowerInclusive = true,
                  bool UpperInclusive = true,
                  typename A          = nullptr_t,
                  typename B          = nullptr_t,
                  typename C          = nullptr_t>
+            requires(type_traits::comparable_between<A, B, C, LowerInclusive, UpperInclusive>)
         CheckResult compareBetween(const A&                    lower,
                                    const B&                    middle,
                                    const C&                    upper,
@@ -169,4 +387,7 @@ namespace bt
             return recordResult(res, loc, std::move(error));
         }
     };
+
+    constexpr CompareMixin::RequireEqualLength RequireEqualLength   = CompareMixin::RequireEqualLength::True;
+    constexpr CompareMixin::RequireEqualLength NoRequireEqualLength = CompareMixin::RequireEqualLength::False;
 }  // namespace bt
--- modules/bettertest/include/bettertest/utils/compare.h
+++ modules/bettertest/include/bettertest/utils/compare.h
@@ -1,64 +1,15 @@
 #pragma once
 
 ////////////////////////////////////////////////////////////////
-// Standard includes.
-////////////////////////////////////////////////////////////////
-
-#include <concepts>
-
-////////////////////////////////////////////////////////////////
 // Current target includes.
 ////////////////////////////////////////////////////////////////
 
 #include "bettertest/utils/result.h"
 #include "bettertest/utils/try.h"
+#include "bettertest/utils/type_traits.h"
 
 namespace bt
 {
-    template<typename A, typename B>
-    concept can_compare_eq = requires(A lhs, B rhs)
-    {
-        {
-            lhs == rhs
-            } -> std::same_as<bool>;
-    };
-
-    template<typename A, typename B>
-    concept can_compare_lt = requires(A lhs, B rhs)
-    {
-        {
-            lhs < rhs
-            } -> std::same_as<bool>;
-    };
-
-    template<typename A, typename B>
-    concept can_compare_gt = requires(A lhs, B rhs)
-    {
-        {
-            lhs > rhs
-            } -> std::same_as<bool>;
-    };
-
-    template<typename A, typename B>
-    concept can_compare_le = requires(A lhs, B rhs)
-    {
-        {
-            lhs <= rhs
-            } -> std::same_as<bool>;
-    };
-
-    template<typename A, typename B>
-    concept can_compare_ge = requires(A lhs, B rhs)
-    {
-        {
-            lhs >= rhs
-            } -> std::same_as<bool>;
-    };
-
-    template<typename A, typename B, typename C, bool L, bool U>
-    concept can_compare_between = (can_compare_le<A, B> && L || can_compare_lt<A, B> && !L) &&
-                                  (can_compare_le<B, C> && U || can_compare_lt<B, C> && !U);
-
     template<typename F>
     result_t compare_impl(const F& f)
     {
@@ -69,56 +20,64 @@ namespace bt
     }
 
     template<typename A>
-    requires(can_compare_eq<A, bool>) result_t compare_true(const A& lhs)
+        requires(type_traits::comparable_eq<A, bool>)
+    result_t compare_true(const A& lhs)
     {
         return compare_impl([&]() { return lhs == true; });
     }
 
     template<typename A>
-    requires(can_compare_eq<A, bool>) result_t compare_false(const A& lhs)
+        requires(type_traits::comparable_eq<A, bool>)
+    result_t compare_false(const A& lhs)
     {
         return compare_impl([&]() { return lhs == false; });
     }
 
     template<typename A, typename B>
-    requires(can_compare_eq<A, B>) result_t compare_eq(const A& lhs, const B& rhs)
+        requires(type_traits::comparable_eq<A, B>)
+    result_t compare_eq(const A& lhs, const B& rhs)
     {
         return compare_impl([&]() { return lhs == rhs; });
     }
 
     template<typename A, typename B>
-    requires(can_compare_eq<A, B>) result_t compare_ne(const A& lhs, const B& rhs)
+        requires(type_traits::comparable_ne<A, B>)
+    result_t compare_ne(const A& lhs, const B& rhs)
     {
         return compare_impl([&]() { return !(lhs == rhs); });
     }
 
     template<typename A, typename B>
-    requires(can_compare_lt<A, B>) result_t compare_lt(const A& lhs, const B& rhs)
+        requires(type_traits::comparable_lt<A, B>)
+    result_t compare_lt(const A& lhs, const B& rhs)
     {
         return compare_impl([&]() { return lhs < rhs; });
     }
 
     template<typename A, typename B>
-    requires(can_compare_gt<A, B>) result_t compare_gt(const A& lhs, const B& rhs)
+        requires(type_traits::comparable_gt<A, B>)
+    result_t compare_gt(const A& lhs, const B& rhs)
     {
         return compare_impl([&]() { return lhs > rhs; });
     }
 
     template<typename A, typename B>
-    requires(can_compare_le<A, B>) result_t compare_le(const A& lhs, const B& rhs)
+        requires(type_traits::comparable_le<A, B>)
+    result_t compare_le(const A& lhs, const B& rhs)
     {
         return compare_impl([&]() { return lhs <= rhs; });
     }
 
     template<typename A, typename B>
-    requires(can_compare_ge<A, B>) result_t compare_ge(const A& lhs, const B& rhs)
+        requires(type_traits::comparable_ge<A, B>)
+    result_t compare_ge(const A& lhs, const B& rhs)
     {
         return compare_impl([&]() { return lhs >= rhs; });
     }
 
     template<typename A, typename B, typename C, bool LowerInclusive, bool UpperInclusive>
-    requires(can_compare_between<A, B, C, LowerInclusive, UpperInclusive>) result_t
-      compare_between(const A& lower, const B& middle, const C& upper)
+        requires(type_traits::comparable_between<A, B, C, LowerInclusive, UpperInclusive>)
+    result_t compare_between(const A& lower, const B& middle, const C& upper)
     {
         if constexpr (LowerInclusive)
         {
